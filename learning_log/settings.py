3a6e7469393bf3177251b7baa91184159019f7c7
joelandrewsimard <simard16@up.edu>
2017-02-25 14:57:17 -0800

    Ready for deployment to heroky.

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -148,8 +148,6 @@ if os.getcwd() =='/app':
     ALLOWED_HOSTS = ['*']
 
     # Static asset config
-    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    STATIC_ROOT = 'staticfiles'
-    STATICFILES_DIRS = {
-        os.path.join(BASE_DIR, 'static'),
-    }
+    PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+    STATIC_URL = '/static/'
+    STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
