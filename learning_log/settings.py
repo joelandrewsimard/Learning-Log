ba400ab554e15c81080f517ad1240fabedd4d4c0
joelandrewsimard <simard16@up.edu>
2017-02-25 14:47:42 -0800

    Ready for deployment to heroku.

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -148,8 +148,11 @@ if os.getcwd() =='/app':
     ALLOWED_HOSTS = ['*']
 
     # Static asset config
-    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    STATIC_ROOT = 'staticfiles'
-    STATICFILE_DIRS = (
-        os.path.join(BASE_DIR, 'static'),
-    )
+    PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+    STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+
+    # BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    # STATIC_ROOT = 'staticfiles'
+    # STATICFILE_DIRS = (
+    #     os.path.join(BASE_DIR, 'static'),
+    # )
